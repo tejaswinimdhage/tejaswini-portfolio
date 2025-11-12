46a06ff4a74bbb933db40380dd3811330187f6bc
tejaswinimdhage <tejaswinimdhage@gmail.com>
2025-11-12 22:26:28 +0530

    projects removed temporarily

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -17,7 +17,7 @@ function Main() {
             <Education />
             <Skills />
             <Experience />
-            <Projects />
+            {/* <Projects /> */}
             <Achievement />
             <Services />
             <Testimonials />
